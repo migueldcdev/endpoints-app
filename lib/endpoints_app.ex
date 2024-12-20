3c1bd5a99d60252ff74b7f6a4507dfc33222c7d5
miguel <migueldc.dev@protonmail.com>
2024-12-20 11:57:57 +0100

    add: endpoints

--- lib/endpoints_app.ex
+++ lib/endpoints_app.ex
@@ -14,15 +14,30 @@ defmodule EndpointsApp do
   """
 
   import Plug.Conn
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
 
   def init(options) do
     options
   end
 
-  def call(conn, _opts) do
+  get "/ping" do
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world\n")
+    |> send_resp(200, "Pong!")
   end
 
+  get "number/:param" do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "#{param}")
+  end
+
+  match _ do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(404, "Endpoint not found")
+  end
 end
