7475fb1fb7dd0e323885c8e0bc39dff42b3800c0
miguel <migueldc.dev@protonmail.com>
2024-12-20 11:17:16 +0100

    add: init & call functions

--- lib/endpoints_app.ex
+++ lib/endpoints_app.ex
@@ -12,7 +12,17 @@ defmodule EndpointsApp do
       :world
 
   """
-  def hello do
-    :world
+
+  import Plug.Conn
+
+  def init(options) do
+    options
   end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello world\n")
+  end
+
 end
